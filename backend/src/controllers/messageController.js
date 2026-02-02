87962759adae47c00440725a11edca8565019073
Ayush Sharma <sharayush1441@gmail.com>
2026-02-02 08:00:42 +0530

    added message controller

---
+++ backend/src/controllers/messageController.js
@@ -0,0 +1,28 @@
+import { Message } from "../models/Message";
+import { Chat } from "../models/Chat";
+
+export async function getMessages(req, res, next) {
+  try {
+    const userId = req.userId;
+    const { chatId } = req.params;
+
+    const chat = await Chat.findOne({
+      _id: chatId,
+      participants: userId,
+    });
+
+    if (!chat) {
+      res.status(404).json({ message: "Chat not found" });
+      return;
+    }
+
+    const messages = await Message.find({ chat: chatId })
+      .populate("sender", "name email avatar")
+      .sort({ createdAt: 1 }); 
+
+    res.json(messages);
+  } catch (error) {
+    res.status(500);
+    next(error);
+  }
+}

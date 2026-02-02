f65254a81922b07c40b0c5fef8bca47771edf21a
Ayush Sharma <sharayush1441@gmail.com>
2026-02-02 08:00:51 +0530

    added user controller

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,16 @@
+import { User } from "../models/User";
+
+export async function getUsers(req, res, next) {
+  try {
+    const userId = req.userId;
+
+    const users = await User.find({ _id: { $ne: userId } })
+      .select("name email avatar")
+      .limit(50);
+
+    res.json(users);
+  } catch (error) {
+    res.status(500);
+    next(error);
+  }
+}

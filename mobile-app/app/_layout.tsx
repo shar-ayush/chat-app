1cd2fac6a99ad900cbb4488884ad495a4fcf202c
Ayush Sharma <sharayush1441@gmail.com>
2026-02-03 22:10:38 +0530

    tanstack query setup

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,7 +1,14 @@
 import { Stack } from "expo-router";
 import "../global.css";
 
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
+  );
 }

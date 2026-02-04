2f5ddd809d517d68445a4aa1965668b0668b0470
Ayush Sharma <sharayush1441@gmail.com>
2026-02-04 18:56:09 +0530

    - Added @clerk/clerk-expo, expo-blur, expo-linear-gradient, and expo-secure-store. - Upgraded react-native-reanimated to version 4.1.1.

---
+++ mobile-app/app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const ChatsTab = () => {
+  return (
+    <SafeAreaView className='bg-surface flex-1'>
+      <Text className='text-white'>index</Text>
+    </SafeAreaView>
+  )
+}
+
+export default ChatsTab
--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,14 +1,22 @@
 import { Stack } from "expo-router";
 import "../global.css";
 
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Stack />
-    </QueryClientProvider>
+    <ClerkProvider tokenCache={tokenCache}>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#0D0D0F' } }}>
+          <Stack.Screen name="(auth)" options={{ animation: "fade"}} />
+          <Stack.Screen name="(tabs)" options={{ animation: "fade"}} />
+        </Stack>
+      </QueryClientProvider>
+    </ClerkProvider>
   );
 }

c53f7726a53500a5e18e469a68943f045f41effe
Ayush Sharma <sharayush1441@gmail.com>
2026-03-24 23:38:19 +0530

    simplify auth middleware

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -8,16 +8,17 @@ export const protectRoute = [
   async (req, res, next) => {
     try {
       const { userId: clerkId } = getAuth(req);
-      // since we call requireAuth() this if check is not necessary
-      // if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" });
+
 
       const user = await User.findOne({ clerkId });
       if (!user) return res.status(404).json({ message: "User not found" });
 
       req.userId = user._id.toString();
+      req.user = user;
 
       next();
     } catch (error) {
+      console.error('Auth middleware error:', error);
       res.status(500);
       next(error);
     }

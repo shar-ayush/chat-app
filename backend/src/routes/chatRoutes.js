8de2b5cfd55c69ce020d25e0c132365da36f9382
Ayush Sharma <sharayush1441@gmail.com>
2026-03-25 01:08:54 +0530

    feat: add unread message count and mark messages as read functionality

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.js";
-import { getChats, getOrCreateChat } from "../controllers/chatController.js";
+import { getChats, getOrCreateChat, markMessagesAsRead } from "../controllers/chatController.js";
 
 const router = Router();
 
@@ -8,5 +8,6 @@ router.use(protectRoute);
 
 router.get("/", getChats);
 router.post("/with/:participantId", getOrCreateChat);
+router.put("/:chatId/read", markMessagesAsRead);
 
 export default router;
